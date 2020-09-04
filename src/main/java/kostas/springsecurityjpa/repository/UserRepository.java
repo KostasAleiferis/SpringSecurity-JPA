84ef0eedb7e03f8580f1a061a74d1be173e4ad70
Kostas Aleiferis <kaleiferis@adacom.com>
2020-09-04 15:57:10 +0300

    first commit

---
+++ src/main/java/kostas/springsecurityjpa/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package kostas.springsecurityjpa.repository;
+
+import kostas.springsecurityjpa.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+   public Optional<User> findByUserName (String userName);
+}
